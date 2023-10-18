84c045d275c8d6deef6b7b8ce370bcfeea67b593
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2023-10-18 16:52:33 +0300

    fix bugs into App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 /** @format */
-import React, { Component } from 'react';
 import './App.css';
 import { Header } from './Layout/Header';
 import { Footer } from './Layout/Footer';
 import { Main } from './Layout/Main';
 
-const App = () => {
+export const App = () => {
   return (
     <>
       <Header />
@@ -15,4 +14,3 @@ const App = () => {
   );
 };
 
-export default App;

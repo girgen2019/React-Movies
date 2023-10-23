aed9f599b6049182c59b6856d8bcbf56769ccaf7
Henadzi Hirylovich <Gir.gen2019@gmail.com>
2023-10-23 12:05:52 +0300

    fix @media into index.css

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -4,7 +4,7 @@ function Button(props) {
   return (
     <>
       <button
-        className="waves-effect waves-light btn search-btn"
+        className="waves-effect waves-light btn search-btn btn-small"
         onClick={() =>
           props.handleChangeSearch(props.paramSearch, props.paramType)
         }
